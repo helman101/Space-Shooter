f700f27fc21fbbe340a0849704098dc3813ea9b9
helman101 <helmanandresortegon@gmail.com>
2021-04-16 11:52:39 +0200

    load player and enemies sprites in BootScene

--- src/scenes/bootScene.js
+++ src/scenes/bootScene.js
@@ -1,4 +1,8 @@
 import Phaser from 'phaser';
+import player from '../assets/fighter.png'
+import fastEnemy from '../assets/fast-enemy.png'
+import normalEnemy from '../assets/normal-enemy.png'
+import chaserEnemy from '../assets/chaser-enemy.png'
 
 class BootScene extends Phaser.Scene {
   constructor() {
@@ -6,11 +10,14 @@ class BootScene extends Phaser.Scene {
   };
 
   preload() {
-
+    this.load.image('sprPlayer', player);
+    this.load.image('sprNormalEnemy', normalEnemy);
+    this.load.image('sprFastEnemy', fastEnemy);
+    this.load.image('sprchaserEnemy', chaserEnemy1)
   };
 
   create() {
-
+    this.scene.start('Game')
   };
 };
 

cc6be807caa9f8b8918fcdffa9c71e7b3372db2d
helman101 <helmanandresortegon@gmail.com>
2021-04-16 12:52:52 +0200

    change player velocity and add player able tomove in gameScene.js

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -11,12 +11,33 @@ class GameScene extends Phaser.Scene {
   }
 
   create() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+
     this.player = new Player(
       this, 
       this.game.config.width * 0.5,
       this.game.config.height * 0.8,
       'sprPlayer'
     );
+
+  }
+
+  update() {
+    this.player.update();
+
+    if (this.cursors.up.isDown) {
+      this.player.moveUp();
+    }
+    else if (this.cursors.down.isDown) {
+      this.player.moveDown();
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player.moveLeft();
+    }
+    else if (this.cursors.right.isDown) {
+      this.player.moveRight();
+    }
   }
 }
 

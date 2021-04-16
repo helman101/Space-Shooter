b0f1e40160000f3b9fa3217e45d66a6a02aa557c
helman101 <helmanandresortegon@gmail.com>
2021-04-16 10:00:13 +0200

    erase raw-loader setup from webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: "./src/game.js",
+  entry: './src/game.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'game.js'
+    filename: 'game.js',
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
+      root: path.resolve(__dirname, '../'),
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true)
+      WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   module: {
@@ -28,24 +28,20 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
+        use: 'file-loader',
       },
       {
         test: /\.mp3$/,
-        loader: 'file-loader'
-      }
-    ]
+        loader: 'file-loader',
+      },
+    ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist')
-  }
+    contentBase: path.join(__dirname, 'dist'),
+  },
 };

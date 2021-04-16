5f32f62b64e07f037a0af7d0ee29ccb422907c8e
helman101 <helmanandresortegon@gmail.com>
2021-04-16 11:03:20 +0200

    add new game configuration to config.js file

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,13 @@ export default {
   type: Phaser.AUTO,
   width: 600,
   height: 800,
-  title: 'Space Avenger'
+  title: 'Space Avenger',
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  pixelart: true,
+  roundPixels: true
 };
